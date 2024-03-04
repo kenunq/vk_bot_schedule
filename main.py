4854ab61889dbc4ba0e6415aa359f13de29c35a1
Ivan Zabaluev <ivaiika@yandex.ru>
2024-03-04 16:32:26 +0300

    formatting

--- main.py
+++ main.py
@@ -5,210 +5,239 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from decouple import config
 
-from services import get_filename_list, get_schedule_for_teacher, get_schedule_for_group, \
-    get_schedule_for_teacher_monday, get_schedule_for_group_monday
+from services import (
+    get_filename_list,
+    get_schedule_for_teacher,
+    get_schedule_for_group,
+    get_schedule_for_teacher_monday,
+    get_schedule_for_group_monday,
+)
 
 
 def sender(id, text):
-    vk_session.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0})
+    vk_session.method("messages.send", {"user_id": id, "message": text, "random_id": 0})
 
 
-def button_send(id,text):
-    vk_session.method('messages.send', {'user_id': id, 'message' : text, 'keyboard': str(keyboard.get_keyboard()), 'random_id': 0})
+def button_send(id, text):
+    vk_session.method(
+        "messages.send",
+        {
+            "user_id": id,
+            "message": text,
+            "keyboard": str(keyboard.get_keyboard()),
+            "random_id": 0,
+        },
+    )
 
 
 def clear_keyboard():
     keyboard.lines = [[]]
-    keyboard.keyboard['buttons'] = keyboard.lines
+    keyboard.keyboard["buttons"] = keyboard.lines
 
 
 def set_value(key, value, id):
-    return vk_session.method('storage.set', {'key': key, 'value': value, 'user_id': id})
+    return vk_session.method("storage.set", {"key": key, "value": value, "user_id": id})
 
 
 def get_value(key, id):
-    return vk_session.method('storage.get', {'key': key, 'user_id': id})
+    return vk_session.method("storage.get", {"key": key, "user_id": id})
+
 
 def send_schedule_teacher(data, id, file_name, find_obj):
     carousel_elements = []
     count = 0
     for _, item in data.items():
-
         if count == 10:
             break
         count += 1
 
-        subject_name = ' '.join(item['Предмет и преподаватель: '].split()[:-2])
-        title = item['Время: ']  # Получаем время из данных
+        subject_name = " ".join(item["Предмет и преподаватель: "].split()[:-2])
+        title = item["Время: "]  # Получаем время из данных
         # Собираем остальные данные в описание
         description = f"{item['Кабинет: ']}\n{subject_name}"[:80]
-        carousel_elements.append({
-            'title': title,  # Время в заголовке
-            'description': description.strip(),  # Описание с остальными данными
-            # 'action': {'type': 'open_link', 'link': 'https://vk.com'},  # Действие при нажатии на карточку
-            'buttons': [{'action': {'type': 'text', 'label': item['Группа: ']}}]  # Кнопка под карточкой
-        })
+        carousel_elements.append(
+            {
+                "title": title,  # Время в заголовке
+                "description": description.strip(),  # Описание с остальными данными
+                # 'action': {'type': 'open_link', 'link': 'https://vk.com'},  # Действие при нажатии на карточку
+                "buttons": [
+                    {"action": {"type": "text", "label": item["Группа: "]}}
+                ],  # Кнопка под карточкой
+            }
+        )
 
-    carousel_template = {
-        'type': 'carousel',
-        'elements': carousel_elements
-    }
+    carousel_template = {"type": "carousel", "elements": carousel_elements}
 
-    vk_session.method("messages.send", {
-        "user_id": id,
-        "message": f"Расписание на {file_name} для {find_obj}",
-        "template": json.dumps(carousel_template),  # Преобразуем структуру карусели в JSON-строку
-        "random_id": 0
-    })
+    vk_session.method(
+        "messages.send",
+        {
+            "user_id": id,
+            "message": f"Расписание на {file_name} для {find_obj}",
+            "template": json.dumps(
+                carousel_template
+            ),  # Преобразуем структуру карусели в JSON-строку
+            "random_id": 0,
+        },
+    )
 
 
 def send_schedule_group(data, id, file_name, find_obj):
     carousel_elements = []
     count = 0
     for _, item in data.items():
-
         if count == 10:
             break
         count += 1
 
-        if item['Предмет и преподаватель: '] not in ('None', None, '', ' '):
-
-            teacher = item['Предмет и преподаватель: '].split()[-2]
-            subject_name = ' '.join(item['Предмет и преподаватель: '].split()[:-2])
-            title = item['Время: ']  # Получаем время из данных
+        if item["Предмет и преподаватель: "] not in ("None", None, "", " "):
+            teacher = item["Предмет и преподаватель: "].split()[-2]
+            subject_name = " ".join(item["Предмет и преподаватель: "].split()[:-2])
+            title = item["Время: "]  # Получаем время из данных
             # Собираем остальные данные в описание
             description = f"{item['Кабинет: ']}\n{subject_name}"[:80]
         else:
-            teacher = 'Хрр..'
-            title = item['Время: ']  # Получаем время из данных
+            teacher = "Хрр.."
+            title = item["Время: "]  # Получаем время из данных
             # Собираем остальные данные в описание
             description = f"Пусто"
 
-        carousel_elements.append({
-            'title': title,  # Время в заголовке
-            'description': description.strip(),  # Описание с остальными данными
-            # 'action': {'type': 'open_link', 'link': 'https://vk.com'},  # Действие при нажатии на карточку
-            'buttons': [{'action': {'type': 'text', 'label': teacher}}]  # Кнопка под карточкой
-        })
+        carousel_elements.append(
+            {
+                "title": title,  # Время в заголовке
+                "description": description.strip(),  # Описание с остальными данными
+                # 'action': {'type': 'open_link', 'link': 'https://vk.com'},  # Действие при нажатии на карточку
+                "buttons": [
+                    {"action": {"type": "text", "label": teacher}}
+                ],  # Кнопка под карточкой
+            }
+        )
 
-    carousel_template = {
-        'type': 'carousel',
-        'elements': carousel_elements
-    }
+    carousel_template = {"type": "carousel", "elements": carousel_elements}
 
-    vk_session.method("messages.send", {
-        "user_id": id,
-        "message": f"Расписание на {file_name} для {find_obj}",
-        "template": json.dumps(carousel_template),  # Преобразуем структуру карусели в JSON-строку
-        "random_id": 0
-    })
+    vk_session.method(
+        "messages.send",
+        {
+            "user_id": id,
+            "message": f"Расписание на {file_name} для {find_obj}",
+            "template": json.dumps(
+                carousel_template
+            ),  # Преобразуем структуру карусели в JSON-строку
+            "random_id": 0,
+        },
+    )
 
 
-vk_session = vk_api.VkApi(token=config('vk_token'))
-longpoll = VkBotLongPoll(vk_session, config('bot_id'))
+vk_session = vk_api.VkApi(token=config("vk_token"))
+longpoll = VkBotLongPoll(vk_session, config("bot_id"))
 session_api = vk_session.get_api()
-keyboard = VkKeyboard() # inline = True для отображения кнопки в сообщении бота, one_time = True скрытия клавиатуры после нажатия
+keyboard = VkKeyboard()  # inline = True для отображения кнопки в сообщении бота, one_time = True скрытия клавиатуры после нажатия
 
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
         if event.from_user:
+            msg = event.object["message"]["text"].lower()
+            id = event.object["message"]["from_id"]
 
-            msg = event.object['message']['text'].lower()
-            id = event.object['message']['from_id']
+            if not get_value("user_states", id):
+                set_value("user_states", "start", id)
 
-            if not get_value('user_states', id):
-                set_value('user_states', 'start', id)
-
-            if msg == 'начать':
-                if get_value('user_states', id) == 'start':
+            if msg == "начать":
+                if get_value("user_states", id) == "start":
                     clear_keyboard()
-                    keyboard.add_button('Расписание', color=VkKeyboardColor.POSITIVE)
+                    keyboard.add_button("Расписание", color=VkKeyboardColor.POSITIVE)
                     user_get = session_api.users.get(user_ids=(id,))
-                    button_send(id, f'Привет {user_get[0]["first_name"]}!\nНапиши "Расписание"')
+                    button_send(
+                        id, f'Привет {user_get[0]["first_name"]}!\nНапиши "Расписание"'
+                    )
                     continue
                 else:
-                    sender(id, 'Вы ввели некорректные данные')
-                    set_value('user_states', 'start', id)
+                    sender(id, "Вы ввели некорректные данные")
+                    set_value("user_states", "start", id)
                     continue
 
-            if msg in ('расписание', 'назад'):
-                    set_value('user_states', 'start', id)
+            if msg in ("расписание", "назад"):
+                set_value("user_states", "start", id)
 
-                    clear_keyboard()
+                clear_keyboard()
 
-                    max_in_line = 2
+                max_in_line = 2
 
-                    files = get_filename_list()
+                files = get_filename_list()
 
-                    count = 0
+                count = 0
 
-                    if len(files) > 20:
-                        max_in_line = 3
+                if len(files) > 20:
+                    max_in_line = 3
 
-                    for i in files:
-                        if i.startswith('График'):
-                            continue
+                for i in files:
+                    if i.startswith("График"):
+                        continue
 
-                        if count == max_in_line:
-                            count = 0
-                            keyboard.add_line()
-                        keyboard.add_button(i, color=VkKeyboardColor.PRIMARY)
-                        count += 1
-                    try:
-                        button_send(id, 'Выбери дату:')
-                    except:
-                        sender(id, 'Что-то пошло не так')
-                    continue
+                    if count == max_in_line:
+                        count = 0
+                        keyboard.add_line()
+                    keyboard.add_button(i, color=VkKeyboardColor.PRIMARY)
+                    count += 1
+                try:
+                    button_send(id, "Выбери дату:")
+                except:
+                    sender(id, "Что-то пошло не так")
+                continue
 
             if not msg[0].isdigit():
                 msg = msg.capitalize()
 
             if msg in get_filename_list():
-                    set_value('file_name', msg, id)
-                    clear_keyboard()
-                    keyboard.add_button('Назад', color=VkKeyboardColor.NEGATIVE)
-                    set_value('user_states', 'enter_date', id)
-                    button_send(id, 'Введите номер группы или фамилию преподавателя:')
-                    continue
+                set_value("file_name", msg, id)
+                clear_keyboard()
+                keyboard.add_button("Назад", color=VkKeyboardColor.NEGATIVE)
+                set_value("user_states", "enter_date", id)
+                button_send(id, "Введите номер группы или фамилию преподавателя:")
+                continue
 
             if msg:
-                if get_value('user_states', id) == 'enter_date':
+                if get_value("user_states", id) == "enter_date":
                     if len(msg) <= 4 and msg[:3].isdigit():
-                        file_name = get_value('file_name', id)
-                        if 'понедельник' in file_name:
-                            schedules = get_schedule_for_group_monday(file_name + '.xlsx', msg)
+                        file_name = get_value("file_name", id)
+                        if "понедельник" in file_name:
+                            schedules = get_schedule_for_group_monday(
+                                file_name + ".xlsx", msg
+                            )
                         else:
-                            schedules = get_schedule_for_group(file_name + '.xlsx', msg)
+                            schedules = get_schedule_for_group(file_name + ".xlsx", msg)
                         if schedules:
                             send_schedule_group(schedules, id, file_name, msg)
-                            sender(id, 'Можете продолжить поиск')
+                            sender(id, "Можете продолжить поиск")
                         else:
-                            sender(id, 'Ничего не найдено')
+                            sender(id, "Ничего не найдено")
                         # set_value('user_states', 'start', id)
                         # continue
                     else:
-                        file_name = get_value('file_name', id)
-                        if 'понедельник' in file_name:
-                            schedules = get_schedule_for_teacher_monday(file_name+'.xlsx', msg.title())
+                        file_name = get_value("file_name", id)
+                        if "понедельник" in file_name:
+                            schedules = get_schedule_for_teacher_monday(
+                                file_name + ".xlsx", msg.title()
+                            )
                         else:
-                            schedules = get_schedule_for_teacher(file_name+'.xlsx', msg.title())
+                            schedules = get_schedule_for_teacher(
+                                file_name + ".xlsx", msg.title()
+                            )
                         if schedules:
                             send_schedule_teacher(schedules, id, file_name, msg.title())
-                            sender(id, 'Можете продолжить поиск')
+                            sender(id, "Можете продолжить поиск")
                             continue
                         else:
-                            button_send(id, 'Ничего не найдено')
+                            button_send(id, "Ничего не найдено")
                         # set_value('user_states', 'start', id)
                         # continue
                 else:
-                    sender(id, 'Вы ввели некорректные данные')
-                    set_value('user_states', 'start', id)
+                    sender(id, "Вы ввели некорректные данные")
+                    set_value("user_states", "start", id)
                     continue
 
     elif event.type == VkBotEventType.MESSAGE_EVENT:
         payload = event.object.payload
-        if payload is not None and 'command' in payload:
-            if payload['command'] == 'button_pressed':
+        if payload is not None and "command" in payload:
+            if payload["command"] == "button_pressed":
                 # Обрабатываем нажатие на кнопку
                 print("Пользователь нажал на кнопку!")
-
--- services.py
+++ services.py
@@ -16,7 +16,9 @@ from openpyxl import load_workbook
 def get_schedule_for_teacher(filename: str, find: str) -> dict:
     """Функция для создания словаря путем парсинга excel таблицы"""
 
-    book = load_workbook(filename=f'{os.getcwd()}/static/schedule/Pасписание/' + filename)
+    book = load_workbook(
+        filename=f"{os.getcwd()}/static/schedule/Pасписание/" + filename
+    )
     ws = book.active
     # namep = (os.path.basename(filename))
     masstime = []
@@ -28,7 +30,7 @@ def get_schedule_for_teacher(filename: str, find: str) -> dict:
         for j in range(2, 34):
             lol = ws.cell(row=j, column=i).value
             if lol is None:
-                lol = '@'
+                lol = "@"
             if isinstance(lol, str):
                 if find in lol:
                     if 3 <= j <= 8:
@@ -40,26 +42,36 @@ def get_schedule_for_teacher(filename: str, find: str) -> dict:
                     elif 24 <= j <= 29:
                         qaz = ws.cell(row=23, column=i).value
                     masstime.insert(0, ws.cell(row=j, column=2).value)
-                    masssur.insert(0, lol.strip().replace('\n', ' '))
+                    masssur.insert(0, lol.strip().replace("\n", " "))
                     massgr.insert(0, qaz)
                     masskb.insert(0, ws.cell(row=j, column=i + 1).value)
 
     lenght = len(masstime)
 
     pre_result = {}
-    #заполняем словарь данными
+    # заполняем словарь данными
     for i in range(lenght):
-        pre_result[i] = {'Время: ': str(masstime[i]), 'Предмет и преподаватель: ': str(masssur[i]),
-                         'Группа: ': str(massgr[i]), 'Кабинет: ': str(masskb[i])}
+        pre_result[i] = {
+            "Время: ": str(masstime[i]),
+            "Предмет и преподаватель: ": str(masssur[i]),
+            "Группа: ": str(massgr[i]),
+            "Кабинет: ": str(masskb[i]),
+        }
 
-    #сортируем спарсенные данные по времени
-    sotr_dict_key = sorted(pre_result, key=lambda x: int(pre_result[x]['Время: '].split('.')[0]))
+    # сортируем спарсенные данные по времени
+    sotr_dict_key = sorted(
+        pre_result, key=lambda x: int(pre_result[x]["Время: "].split(".")[0])
+    )
 
-    #перезаписываем отсортированные данные в словарь
+    # перезаписываем отсортированные данные в словарь
     result = {}
     for i in range(lenght):
-        result[i] = {'Время: ': str(masstime[sotr_dict_key[i]]), 'Предмет и преподаватель: ': str(masssur[sotr_dict_key[i]]),
-                     'Группа: ': str(massgr[sotr_dict_key[i]]), 'Кабинет: ': str(masskb[sotr_dict_key[i]])}
+        result[i] = {
+            "Время: ": str(masstime[sotr_dict_key[i]]),
+            "Предмет и преподаватель: ": str(masssur[sotr_dict_key[i]]),
+            "Группа: ": str(massgr[sotr_dict_key[i]]),
+            "Кабинет: ": str(masskb[sotr_dict_key[i]]),
+        }
 
     return result
 
@@ -67,7 +79,9 @@ def get_schedule_for_teacher(filename: str, find: str) -> dict:
 @cache
 def get_schedule_for_teacher_monday(filename: str, find: str) -> dict:
     """Функция для парсинга расписания для преподавателя в понедельник"""
-    book = load_workbook(filename=f'{os.getcwd()}/static/schedule/Pасписание/' + filename)
+    book = load_workbook(
+        filename=f"{os.getcwd()}/static/schedule/Pасписание/" + filename
+    )
     ws = book.active
     result = {}
     count = 0
@@ -75,27 +89,35 @@ def get_schedule_for_teacher_monday(filename: str, find: str) -> dict:
         for j in range(2, 38):
             obj = str(ws.cell(row=j, column=i).value)
             if find in obj:
-                group = 'Не обнаружено'
+                group = "Не обнаружено"
                 for x in range(1, 8):
                     if str(ws.cell(row=j - x, column=i).value)[:3].isdigit():
                         group = str(ws.cell(row=j - x, column=i).value)
                         break
                 result[count] = {
-                    'Время: ': str(ws.cell(row=j, column=2).value),
-                    'Предмет и преподаватель: ': str(ws.cell(row=j, column=i).value).replace('\n', ' '),
-                    'Группа: ': group,
-                    'Кабинет: ': str(ws.cell(row=j, column=i + 1).value) if 'Россия' not in obj else str(ws.cell(row=j-1, column=i + 1).value)
+                    "Время: ": str(ws.cell(row=j, column=2).value),
+                    "Предмет и преподаватель: ": str(
+                        ws.cell(row=j, column=i).value
+                    ).replace("\n", " "),
+                    "Группа: ": group,
+                    "Кабинет: ": str(ws.cell(row=j, column=i + 1).value)
+                    if "Россия" not in obj
+                    else str(ws.cell(row=j - 1, column=i + 1).value),
                 }
                 count += 1
 
-    sorted_schedule = dict(sorted(result.items(), key=lambda x: int(x[1]['Время: '].split('.')[0])))
+    sorted_schedule = dict(
+        sorted(result.items(), key=lambda x: int(x[1]["Время: "].split(".")[0]))
+    )
     return sorted_schedule
 
 
 @cache
 def get_schedule_for_group(filename: str, find: str) -> dict:
     """Функция для создания словаря путем парсинга excel таблицы"""
-    book = load_workbook(filename=f'{os.getcwd()}/static/schedule/Pасписание/' + filename)
+    book = load_workbook(
+        filename=f"{os.getcwd()}/static/schedule/Pасписание/" + filename
+    )
     ws = book.active
     SCHEDULE_LEN = 6
     result = {}
@@ -104,10 +126,14 @@ def get_schedule_for_group(filename: str, find: str) -> dict:
             obj = str(ws.cell(row=j, column=i).value)
             if obj == find:
                 for iter in range(SCHEDULE_LEN):
-                    result[iter] = {'Время: ': str(ws.cell(row=j+iter+1, column=2).value),
-                                 'Предмет и преподаватель: ': str(ws.cell(row=j+iter+1, column=i).value).replace('\n', ' '),
-                                 'Группа: ': obj,
-                                 'Кабинет: ': str(ws.cell(row=j+iter+1, column=i+1).value)}
+                    result[iter] = {
+                        "Время: ": str(ws.cell(row=j + iter + 1, column=2).value),
+                        "Предмет и преподаватель: ": str(
+                            ws.cell(row=j + iter + 1, column=i).value
+                        ).replace("\n", " "),
+                        "Группа: ": obj,
+                        "Кабинет: ": str(ws.cell(row=j + iter + 1, column=i + 1).value),
+                    }
 
     return result
 
@@ -115,19 +141,29 @@ def get_schedule_for_group(filename: str, find: str) -> dict:
 @cache
 def get_schedule_for_group_monday(filename: str, find: str) -> dict:
     """Функция для создания словаря путем парсинга excel таблицы"""
-    book = load_workbook(filename=f'{os.getcwd()}/static/schedule/Pасписание/' + filename)
+    book = load_workbook(
+        filename=f"{os.getcwd()}/static/schedule/Pасписание/" + filename
+    )
     ws = book.active
-    SCHEDULE_LEN = 7
+    SCHEDULE_LEN = 8
     result = {}
     for i in range(1, 18):
         for j in range(2, 34):
             obj = str(ws.cell(row=j, column=i).value)
             if obj == find:
                 for iter in range(SCHEDULE_LEN):
-                    result[iter] = {'Время: ': str(ws.cell(row=j+iter+1, column=2).value),
-                                 'Предмет и преподаватель: ': str(ws.cell(row=j+iter+1, column=i).value).replace('\n', ' '),
-                                 'Группа: ': obj,
-                                 'Кабинет: ': str(ws.cell(row=j+iter+1, column=i+1).value) if 'Россия' not in str(ws.cell(row=j+iter+1, column=i).value).replace('\n', ' ') else str(ws.cell(row=j+iter, column=i+1).value)
+                    result[iter] = {
+                        "Время: ": str(ws.cell(row=j + iter + 1, column=2).value),
+                        "Предмет и преподаватель: ": str(
+                            ws.cell(row=j + iter + 1, column=i).value
+                        ).replace("\n", " "),
+                        "Группа: ": obj,
+                        "Кабинет: ": str(ws.cell(row=j + iter + 1, column=i + 1).value)
+                        if "Россия"
+                        not in str(ws.cell(row=j + iter + 1, column=i).value).replace(
+                            "\n", " "
+                        )
+                        else str(ws.cell(row=j + iter, column=i + 1).value),
                     }
 
     return result
@@ -136,11 +172,11 @@ def get_schedule_for_group_monday(filename: str, find: str) -> dict:
 def get_filename_list() -> list[str]:
     """Функция для получения доступных расписаний занятий"""
 
-    list_name = os.listdir(os.getcwd() + '/static/schedule/Pасписание')
+    list_name = os.listdir(os.getcwd() + "/static/schedule/Pасписание")
 
     result = []
     for i in list_name:
-        result.append(i.replace('.xlsx', '').strip())
+        result.append(i.replace(".xlsx", "").strip())
 
     return result
 
@@ -148,25 +184,25 @@ def get_filename_list() -> list[str]:
 def check_on_update(url: str) -> bool:
     """Функция для проверки наличия обновлений"""
 
-    st_accept = 'text/html'
+    st_accept = "text/html"
 
-    st_useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    st_useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 
-    headers = {'User-Agent': st_useragent, 'Accept': st_accept}
+    headers = {"User-Agent": st_useragent, "Accept": st_accept}
 
     response = requests.get(url, headers=headers)
 
     src = response.text
 
-    soup = BeautifulSoup(src, 'html.parser')
+    soup = BeautifulSoup(src, "html.parser")
 
-    title_elements = soup.find_all(class_='listing-item__title')
+    title_elements = soup.find_all(class_="listing-item__title")
 
     result_parse = []
 
     # Проходимся по найденным элементам и выводим содержимое каждого span
     for title_element in title_elements:
-        span_element = title_element.find('span')
+        span_element = title_element.find("span")
         if span_element:
             result_parse.append(span_element.text[:-5].rstrip())
 
@@ -193,13 +229,13 @@ def del_from_folder() -> None:
 def download_exel(url: str) -> None:
     """Функция для скачивания excel таблиц с расписанием"""
 
-    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+    base_url = "https://cloud-api.yandex.net/v1/disk/public/resources/download?"
     public_key = url
 
     # Получаем загрузочную ссылку
     final_url = base_url + urlencode(dict(public_key=public_key))
     response = requests.get(final_url)
-    download_url = response.json()['href']
+    download_url = response.json()["href"]
     to_url = os.path.dirname(os.path.abspath(__file__))
 
     # записываем в переменную бинарные данные
@@ -207,31 +243,30 @@ def download_exel(url: str) -> None:
     datatowrite = filedata.read()
 
     # переводим бинарные данные в зип файл
-    with open(to_url + '/static/schedule/qwe.zip', 'wb') as f:
+    with open(to_url + "/static/schedule/qwe.zip", "wb") as f:
         f.write(datatowrite)
 
     # разархивируем архив
-    with ZipFile(to_url + '/static/schedule/qwe.zip', 'r') as zip_file:
-        zip_file.extractall(to_url + '/static/schedule')
-        os.remove(to_url + '/static/schedule/qwe.zip')
+    with ZipFile(to_url + "/static/schedule/qwe.zip", "r") as zip_file:
+        zip_file.extractall(to_url + "/static/schedule")
+        os.remove(to_url + "/static/schedule/qwe.zip")
 
-    file_list = os.walk(os.getcwd() + '/static/schedule/Pасписание')
+    file_list = os.walk(os.getcwd() + "/static/schedule/Pасписание")
     for root, dirs, files in file_list:
         for file in files:
             file_path = os.path.join(root, file)
-            new_file_name = file[:-5].rstrip() + '.xlsx'
+            new_file_name = file[:-5].rstrip() + ".xlsx"
             new_file_path = os.path.join(root, new_file_name)
             os.rename(file_path, new_file_path)
 
 
 class AsyncActionDownloadSchedule(threading.Thread):
-    '''Класс работающий не в основном потоке, который качает excel таблиц с рассписанием'''
+    """Класс работающий не в основном потоке, который качает excel таблиц с рассписанием"""
 
-    url_schedule = 'https://disk.yandex.ru/d/VNdnX6hmveqJuw'
+    url_schedule = "https://disk.yandex.ru/d/VNdnX6hmveqJuw"
 
     def run(self):
         while True:
-
             # Проверяем наличие обновлений
             if not check_on_update(self.url_schedule):
                 # таймаут в 15 минут
